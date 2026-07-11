68efba5d7f6e37667d97b79dbfde3c4c5de78b8b
taniya221 <taniya.jaison007@gmail.com>
2026-07-11 09:48:35 +0530

    student entry completed

---
+++ src/components/AddStudent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AddStudent = () => {
+  return (
+    <div>
+        <h1>
+            StudentEntry
+        </h1>
+    </div>
+  )
+}
+
+export default AddStudent
