bbae86a9dec98d70d2e24e25dbff0eb5aef416dd
taniya221 <taniya.jaison007@gmail.com>
2026-07-11 10:49:54 +0530

    view feature added

---
+++ src/components/ViewStudent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ViewStudent = () => {
+  return (
+    <div>
+        
+        <h1>
+            View Student
+        </h1>
+    </div>
+  )
+}
+
+export default ViewStudent
